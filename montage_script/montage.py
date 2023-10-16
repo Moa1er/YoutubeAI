26b7065deef8942317173111b62e3851f172c504
Moa1er <twoswaglol.law@gmail.com>
2023-10-16 12:46:02 +0900

    add vad

--- montage_script/montage.py
+++ montage_script/montage.py
@@ -1,9 +1,45 @@
 import os
+import subprocess
+import sys
 from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips # pip3 install moviepy
+# NEED : sudo apt install ffmpeg
+from montage_script.vad import get_voice_time
+from os import path
+
 
 def make_montage(video_file_path, audio_file_path, artefact_file_path):
-    video_clip = VideoFileClip(video_file_path)
-    audio_clip = AudioFileClip(audio_file_path)
 
-    final_clip = video_clip.set_audio(audio_clip)
-    final_clip.write_videofile(artefact_file_path)
+    # separation audio from video in tiktok downloaded clip
+    video_path_attr = video_file_path.split(".") #[0] name, [1] extension
+    video_audio_name = video_path_attr[0] + "_video_sound.mp3"
+    convert_video_to_audio_ffmpeg(video_file_path, video_audio_name)
+
+    time_to_cut = get_voice_time(video_audio_name)
+    print("time_to_cut", time_to_cut)
+
+    # video_clip = VideoFileClip(video_file_path)
+    # audio_clip = AudioFileClip(audio_file_path)
+
+    # final_clip = video_clip.set_audio(audio_clip)
+    # final_clip.write_videofile(artefact_file_path)
+
+
+def convert_video_to_audio_ffmpeg(video_file, video_audio_name):
+    """Converts video to audio directly using `ffmpeg` command
+    with the help of subprocess module"""
+    filename, ext = os.path.splitext(video_file)
+    subprocess.call(["ffmpeg", "-y", "-i", video_file, video_audio_name], 
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.STDOUT)
+    
+# from pydub import AudioSegment 
+  
+# song = AudioSegment.from_mp3("test.mp3")
+# # pydub does things in milliseconds 
+# ten_seconds = 10 * 1000
+  
+# first_10_seconds = song[:ten_seconds] 
+  
+# last_5_seconds = song[-5000:] 
+
+# first_10_seconds.export("new.mp3", format="mp3") 
--- youtube-ai.py
+++ youtube-ai.py
@@ -17,11 +17,10 @@ def main():
 
     #TESTED
     # gets video title and url + aweme_id from tiktok
-    trend_keyword = "dance"
-    vid_title, vid_url, aweme_id = get_vid_name_url_id(trend_keyword, NB_VOD_TO_COMP)
-    print("Video found: " + vid_title)
-    print("Vod URL: " + vid_url)
-    print("Vid id: " + aweme_id)
+    # vid_title, vid_url, aweme_id = get_vid_name_url_id(trend_keyword, NB_VOD_TO_COMP)
+    # print("Video found: " + vid_title)
+    # print("Vod URL: " + vid_url)
+    # print("Vid id: " + aweme_id)
 
     # TESTED
     # gets comments from tiktok video
@@ -32,16 +31,13 @@ def main():
     # artefacts_file_name = trend_keyword + "_" + datetime.datetime.now().strftime("%Y-%m-%d")
     # print("artefacts_file_name: " + artefacts_file_name)
     
-    # TESTED but no sound ? TODO
-    # vid_url = "https://v39.tiktokcdn-us.com/f4d18caa7c5a8a86d1273727114f5db3/652bd60d/video/tos/useast5/tos-useast5-pve-0068-tx/oQTqIWVcyIpBCHBD1ANfBZphUhzQSuAciwNMgo/?a=1233&ch=0&cr=13&dr=0&lr=all&cd=0%7C0%7C0%7C&cv=1&br=1222&bt=611&bti=NEBzNTY6QGo0NzZALnAjNDQuYCM1NTNg&cs=2&ds=6&ft=_rbUxBnZq8Zmo.xS.Q_vjmpEnreLrus&mime_type=video_mp4&qs=11&rc=NzNkPDo2aDU7ZDs4Z2U7Z0Bpam04ZDY6ZmtnazMzZzczNEA2M15iNF4uXjYxYF9jYzQwYSNebDE2cjQwNmlgLS1kMS9zcw%3D%3D&l=20231015060726EF189E356CA5B5668CA7&btag=e00008000"
-    # video_file_path = ASSETS_FOLDER + "tmp_hacks_2023-10-14.mp4"
-
-    # # gets first artefact "trend_keyword_tmp_date.mp4"
+    # TESTED
+    # gets first artefact "trend_keyword_tmp_date.mp4"
     # video_file_path = ASSETS_FOLDER + "tmp_" + artefacts_file_name + ".mp4"
     # download_vod(vid_url, video_file_path)
 
     # TESTED
-    # # gets impression text
+    # gets impression text
     # impression, new_vid_title = get_impression_and_title(vid_title, ",".join(vid_comments))
     # new_vid_title += " #Shorts"
     # print("impression: " + impression)
@@ -53,6 +49,7 @@ def main():
     # text_to_speech(impression, audio_file_path)
 
     
+    video_file_path = ASSETS_FOLDER + "tmp_hacks_2023-10-14.mp4"
     audio_file_path = ASSETS_FOLDER + "tmp_hacks_2023-10-14.mp3"
     final_vid_file_path = ASSETS_FOLDER + "hacks_2023-10-14.mp4"
     new_vid_title = "#LifeHackMagic: Unbelievable Tips That Will Blow Your Mind!"
@@ -61,7 +58,7 @@ def main():
     # TESTED
     # gets third artefact "trend_keyword_date.mp4"
     # final_vid_file_path = ASSETS_FOLDER + artefacts_file_name + ".mp4"
-    # make_montage(video_file_path, audio_file_path, final_vid_file_path)
+    make_montage(video_file_path, audio_file_path, final_vid_file_path)
 
     # TESTED
     # uploads to youtube
